954da81b4c3feb455bcae86f033448f3d1efd693
Payapalay14 <payapalay14@example.com>
2026-01-29 23:54:08 +0630

    add dummy web server for render

--- bot.py
+++ bot.py
@@ -10,4 +10,16 @@ def start(message):
 
 print("Bot is running...")
 bot.infinity_polling()
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'မင်္ဂလာပါ https://t.me/jackgyi111')
 
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message, 'အကူအညီ https://t.me/jackgyi111')
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
